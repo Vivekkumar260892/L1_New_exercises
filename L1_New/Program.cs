4bc0906cfc95d495fa851401970da8987fc5a6b6
Vivekkumar260892 <vivekkumar1@mail.usf.edu>
2019-01-15 21:14:28 -0500

    Exercises

--- L1_New/Program.cs
+++ L1_New/Program.cs
@@ -6,7 +6,22 @@ namespace L1_New
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Exercise 2// Input a number (n) from the user and check if it is even or odd.// Write your code here
+            Console.WriteLine("Input a number : ");
+            string Input = Console.ReadLine();
+            int numbr_1 = int.Parse(Input);
+            if (numbr_1%2==0)
+            { Console.WriteLine("Even number");
+            }
+            else
+            {
+                Console.WriteLine("odd number");
+            }
+            Console.ReadKey();
+
+
+
+
         }
     }
 }
